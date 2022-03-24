366928b817933d1b577eb066a02aca4b312485d2
Maxon <maxsonic99@gmail.com>
2022-03-24 23:31:39 +0300

    first commit

--- test.py
+++ test.py
@@ -4,5 +4,7 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 
 network = core.NeuralNetwork([784, 40, 10])
-network.fit(training_data, 30, 10, 3.0)
+network.fit(training_data, 10, 24, 3.0)
+
+
 print('Accuracy: {} %'.format((network.accuracy(test_data) / len(test_data)) * 100))
